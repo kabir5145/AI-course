2dad97f5bbc17ebbbb0cbf75dadf23a87c67086e
kabir5145 <kabir10112006@gmail.com>
2025-08-25 12:16:44 +0000

    Assignment Statements

--- part_4.py
+++ part_4.py
@@ -9,3 +9,18 @@ print(D / M)
 numerator = 11 + 19
 denominator = 19 + 31
 print(numerator / denominator)
+
+# Assignment Statements
+# Example 1
+B = 19 + 31
+print(B)
+
+# Example 2
+x = 2
+y = x
+print(y)
+
+# Example 3
+t = 2
+t = t + 1
+print(t)
