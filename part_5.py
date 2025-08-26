6167c5fc9542a925f584629aa87b3856c28fb528
kabir5145 <kabir10112006@gmail.com>
2025-08-26 11:30:31 +0000

    Swapping  two variables(incorrect)

--- part_5.py
+++ part_5.py
@@ -2,3 +2,14 @@ x = 25 // 2
 y = x
 x = 25 / 2
 print(y)
+
+# Swapping two variables (incorrect)
+# step 1 = Initialize two variables
+a = 7.5  # float
+b = -12  # integer
+
+# step 2 = Swap the variables
+a = b
+b = a
+
+print(a)
