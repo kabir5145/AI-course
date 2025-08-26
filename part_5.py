0fa72a2466440720851901d6c21f663681bbe4b6
kabir5145 <kabir10112006@gmail.com>
2025-08-26 11:37:02 +0000

    Swapping two variables(correct)

--- part_5.py
+++ part_5.py
@@ -13,3 +13,20 @@ a = b
 b = a
 
 print(a)
+
+# Swapping two variables (correct)
+# Step 1 = Initialize two variables
+c = 7.5  # float
+d = -12  # integer
+
+# Step 2 = Swap them using a temporary variable
+temp = c
+c = d
+d = temp
+
+print(temp)
+
+# Swap them without using a temporary variable
+c, d = d, c
+
+print(c)
